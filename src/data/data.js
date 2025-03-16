47047f87ea5835de0e7a547f00652ceb675e4012
Mary <32392607+MartFrida@users.noreply.github.com>
2025-03-16 17:32:37 +0100

    project-link

--- src/data/data.js
+++ src/data/data.js
@@ -17,7 +17,7 @@ export const dataWorks = [
     name: "Articles Blog with Authorization",
     description:
       "Utilizing 'Backend for Articles Blog', and React, JS, Reduxjs, axios, redux-persist, react-router-dom, react-hook-form, TilewindCSS for development, with deployment handled on Netlify",
-    linkNetlify: "https://merry-gaufre-53e833.netlify.app/",
+    linkNetlify: "article-blog-frontend.netlify.app",
     linkGitLub: "https://github.com/MartFrida/articles-frontend",
   },
   {
